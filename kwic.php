40405f1266f25e24df88dfd8a425afe7c6fb6649
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2012-03-08 10:16:34 +0100

    FIX: Authentification needs headers hiding

---
+++ kwic.php
@@ -0,0 +1,9 @@
+<?php
+  include 'http.php';
+  $corpus = $_GET['corpus'];
+  $start = $_GET['pole'];
+  $end = $start.'\ufff0';
+  $location = substr(BASE_URL,0,-5).'list/kwic/kwic?startkey=["'.$corpus.'","'.$start.'"]&endkey=["'.$corpus.'","'.$end.'"]';
+  $contents = file_get_contents($location);
+  echo $contents;
+?>
