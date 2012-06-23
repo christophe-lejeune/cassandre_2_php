d3d67108477076cf4302afb53ce611057da1d355
Christophe Lejeune <christophe.lejeune@ulg.ac.be>
2012-06-23 18:49:08 +0200

    FIX: Kwic can be computed from phrases or compounds

--- kwic.php
+++ kwic.php
@@ -1,7 +1,7 @@
 <?php
   include 'http.php';
   $corpus = $_GET['corpus'];
-  $start = $_GET['pole'];
+  $start = rawurlencode($_GET['pole']);
   $end = $start.'\ufff0';
   $location = substr(BASE_URL,0,-5).'list/kwic/kwic?startkey=["'.$corpus.'","'.$start.'"]&endkey=["'.$corpus.'","'.$end.'"]';
   $contents = file_get_contents($location);
